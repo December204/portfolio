47729299ee85a5686b186b2b5535843f46a1c0fb
tai <taipm12122004@gmail.com>
2026-01-21 16:21:03 +0700

    portfolio

---
+++ src/layout/Navbar.tsx
@@ -0,0 +1,35 @@
+const navLinks = [
+  { href: "#about", label: "About" },
+  { href: "#project", label: "Project" },
+  { href: "#experience", label: "Experience" },
+  { href: "#testimonials", label: "Testimonial" },
+];
+
+export const Navbar = () => {
+  return (
+    <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
+      <nav className="container mx-auto px-6 flex items-center justify-between">
+        <a
+          href="#"
+          className="text-xl font-bold tracking-tight hover:text-primary"
+        >
+          PM<span className="hover: text-primary">.</span>
+        </a>
+        {/* Desktop nav*/}
+        <div className="flex items-center gap-1">
+          <div className="glass rounded-full px-2 py-1 flex items-center gap-1">
+            {navLinks.map((link, index) => (
+              <a
+                href="{link.href}"
+                key={index}
+                className="px-4 py-2 text-sm text-muted-foreground hover:text-foreground rounded-full"
+              >
+                {link.label}
+              </a>
+            ))}
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
